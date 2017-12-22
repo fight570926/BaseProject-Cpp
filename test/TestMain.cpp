7a4df4ca3160381d5566f1183355da46769f46c3
D0588516 <a0983016265@gmail.com>
2017-12-22 18:57:38 +0800

    D0588516

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -3,6 +3,15 @@
 
 void testGetTriangleType() {
     assert(getTriangleType(3, 4, 5) == 0);
+    assert(getTriangleType(5, 12, 13) == 0);
+    assert(getTriangleType(7, 24, 25) == 0);
+    assert(getTriangleType(20, 21, 29) == 0);
+    assert(getTriangleType(3, 4, 6) == -1);
+    assert(getTriangleType(3, 4, 7) == -2);
+    assert(getTriangleType(3, 4, 8) == -2);
+    assert(getTriangleType(3, 4, 9) == -2);
+    assert(getTriangleType(5, 12, 11) == 1);
+    assert(getTriangleType(5, 12, 12) == 1);
 }
 
 int main() {
